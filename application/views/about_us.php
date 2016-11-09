0aab203d0dd63ff2df4ab341129470ac25e1c879
RolandoContreras <software.contreras@gmail.com>
2016-11-09 15:19:24 -0500

    creation htacces

---
+++ application/views/about_us.php
@@ -0,0 +1,328 @@
+
+<!DOCTYPE html>
+<html lang="en" class="wide wow-animation">
+<head>
+    <!-- Site Title -->
+    <title>About</title>
+    <meta name="format-detection" content="telephone=no"/>
+    <meta name="viewport"
+          content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+
+    <!-- Stylesheets -->
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link href='//fonts.googleapis.com/css?family=Roboto:300,300italic%7CMontserrat:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/style.css">
+
+    <!--[if lt IE 10]>
+    <div style='background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/..">
+            <img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820"
+                 alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+        </a>
+    </div>
+    <script src="js/html5shiv.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<!-- The Main Wrapper -->
+<div class="page">
+    <!--========================================================
+                              HEADER
+    =========================================================-->
+    <header class="page-header">
+        <!-- RD Navbar -->
+        <div class="rd-navbar-wrap">
+            <nav class="rd-navbar" data-rd-navbar-lg="rd-navbar-static">
+                <div class="rd-navbar-inner">
+                    <!-- RD Navbar Panel -->
+                    <div class="rd-navbar-panel">
+
+                        <!-- RD Navbar Toggle -->
+                        <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar"><span></span></button>
+                        <!-- END RD Navbar Toggle -->
+
+                        <!-- RD Navbar Brand -->
+                        <div class="rd-navbar-brand">
+                            <img src="images/logo.png" class="brand-logo" alt="" width="78" height="40">
+                            <a href="index.html" class="brand-name">
+                                C<span>o</span>nnect
+                            </a>
+                        </div>
+                        <!-- END RD Navbar Brand -->
+                    </div>
+                    <!-- END RD Navbar Panel -->
+
+                    <div class="rd-navbar-info box box-skin-1">
+                        <div class="box__left"><div class="icon icon-xs icon-contrast fa fa-phone-square"></div></div>
+                        <div class="box__body"><a href="callto:#">800 555 7744</a></div>
+                    </div>
+
+                    <div class="rd-navbar-nav-wrap">
+
+                        <!-- RD Navbar Nav -->
+                        <ul class="rd-navbar-nav">
+                            <li>
+                                <a href="index.html">Home</a>
+                                <!-- RD Navbar Megamenu -->
+                                <ul class="rd-navbar-megamenu">
+                                    <li>
+                                        <h1>Our Advantages</h1>
+                                    </li>
+                                    <li>
+                                        <img src="images/megamenu-1.jpg" alt="" width="343" height="177">
+                                        <h4>Affordable Costs</h4>
+                                        <p>We set low prices for our services to make telecommunications affordable for the global population.</p>
+                                        <a href="#" class="btn btn-md btn-primary">Learn More</a>
+                                    </li>
+                                    <li>
+                                        <img src="images/megamenu-2.jpg" alt="" width="343" height="177">
+                                        <h4>Diverse Orientation</h4>
+                                        <p>Our services are oriented towards governmental agencies, commercial enterprises, and housing estates.</p>
+                                        <a href="#" class="btn btn-md btn-primary">Learn More</a>
+                                    </li>
+                                    <li>
+                                        <img src="images/megamenu-3.jpg" alt="" width="343" height="177">
+                                        <h4>Significant Discounts</h4>
+                                        <p>Our customers who make a repeat order of our services get a 30% discount.</p>
+                                        <a href="#" class="btn btn-md btn-primary">Learn More</a>
+                                    </li>
+                                </ul>
+                                <!-- END RD Navbar Megamenu -->
+                            </li>
+                            <li class="active">
+                                <a href="index-1.html">About</a>
+
+                                <!-- RD Navbar Dropdown -->
+                                <ul class="rd-navbar-dropdown">
+                                    <li>
+                                        <a href="shortcodes.php">Our Principles</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Latest News</a>
+                                        <ul class="rd-navbar-dropdown">
+                                            <li>
+                                                <a href='#'>Featured Events</a>
+                                            </li>
+                                            <li>
+                                                <a href='#'>Useful links</a>
+                                            </li>
+                                            <li>
+                                                <a href='#'>Special Offers</a>
+                                            </li>
+                                            <li>
+                                                <a href='#'>What’s New?</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#">Special Offers</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Forum</a>
+                                    </li>
+                                </ul>
+                                <!-- END RD Navbar Dropdown -->
+
+                            </li>
+                            <li>
+                                <a href="index-2.html">Services</a>
+                            </li>
+                            <li>
+                                <a href="index-3.html">Gallery</a>
+                            </li>
+                            <li>
+                                <a href="index-4.html">Contacts</a>
+                            </li>
+                        </ul>
+                        <!-- END RD Navbar Nav -->
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <!-- END RD Navbar -->
+    </header>
+    <!--========================================================
+                              CONTENT
+    =========================================================-->
+    <main class="page-content">
+        <!-- Our mission -->
+        <section class="well-sm well-sm-inset-1">
+            <div class="container">
+                <h1>Our Mission</h1>
+                <div class="row">
+                    <div class="col-md-4 text-center">
+                        <img src="images/page-02_img01.jpg" alt="" width="370" height="279">
+                    </div>
+                    <div class="col-md-8">
+                        <p>Our company presents the classical business in the sphere of high technologies. We know how to appreciate our clients and improve our services.</p>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <ul class="marked-list">
+                                    <li><a href="#">Inbound Call Centre</a></li>
+                                    <li><a href="#">Telemarketing Services</a></li>
+                                    <li><a href="#">Lead Capture Services</a></li>
+                                    <li><a href="#">Advertising Campaign</a></li>
+                                    <li><a href="#">Sales Lead Conversion</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-6">
+                                <ul class="marked-list">
+                                    <li><a href="#">Web Enabled Call Centre</a></li>
+                                    <li><a href="#">Real Estate Lead Collection</a></li>
+                                    <li><a href="#">Customer Service Call Centre</a></li>
+                                    <li><a href="#">Technical translation</a></li>
+                                    <li><a href="#">Financial translation</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- END Our mission-->
+
+        <!-- Work team -->
+        <section class="well-md well-md-inset-1 bg-primary text-center">
+            <div class="container wow fadeInUp">
+                <h1>Work Team</h1>
+                <div class="row flow-offset-1">
+                    <div class="col-xs-6 col-md-3">
+                        <img src="images/page-02_img02.jpg" alt="" width="270" height="242">
+                        <h4><a href="#">Eva Adamson</a></h4>
+                        <p>Support Lead, Account</p>
+                        <p>Executive</p>
+                    </div>
+
+                    <div class="col-xs-6 col-md-3">
+                        <img src="images/page-02_img03.jpg" alt="" width="270" height="242">
+                        <h4><a href="#">David Austin</a></h4>
+                        <p>Marketing and Thought</p>
+                        <p>Leadership</p>
+                    </div>
+
+                    <div class="col-xs-6 col-md-3">
+                        <img src="images/page-02_img04.jpg" alt="" width="270" height="242">
+                        <h4><a href="#">Natalie Barnes</a></h4>
+                        <p>Programming and Systems</p>
+                        <p>Manager</p>
+                    </div>
+
+                    <div class="col-xs-6 col-md-3">
+                        <img src="images/page-02_img05.jpg" alt="" width="270" height="242">
+                        <h4><a href="#">Eva Adamson</a></h4>
+                        <p>Marketing, Project</p>
+                        <p>Manager</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- END Work team-->
+
+        <!-- Why Us -->
+        <section class="relative well-lg well-lg-inset-1">
+            <div class="absolute wow fadeInRight">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="image-wrap"><img src="images/page-02_img06.jpg" width="1010" height="1125" alt=""></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container wow fadeInLeft">
+                <div class="row">
+                    <div class="col-md-preffix-7 col-md-5">
+                        <h1>Why Us</h1>
+                        <p class="inset-2">Our company is your one stop solution for all needs. <br>There is no doubt that we are the leaders and you don’t have to worry about our image because it is perfect. A great number of different grateful clients is the best prove of the previous statement.</p>
+                        <a href="#" class="btn btn-primary btn-md">Explore More</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- END Why Us-->
+
+        <!-- Products -->
+        <section class="well-md well-md-inset-1 bg-primary text-center">
+            <div class="container wow fadeInUp">
+                <h1>Products</h1>
+                <ul class="index-list row">
+                    <li class="col-md-4">
+                        <div class="box">
+                            <div class="box__left">
+                                <div class="icon icon-sm icon-secondary-1 index-list__counter"></div>
+                            </div>
+                            <div class="box__body">
+                                <h4><a href="#">Internet</a></h4>
+                                <p>You get the fastest, most reliable, unlimited Internet in the area.</p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="col-md-4">
+                        <div class="box">
+                            <div class="box__left">
+                                <div class="icon icon-sm icon-secondary-1 index-list__counter"></div>
+                            </div>
+                            <div class="box__body">
+                                <h4><a href="#">Digital TV</a></h4>
+                                <p>You want a wide selction of Digital TV choices and we deliver!</p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="col-md-4">
+                        <div class="box">
+                            <div class="box__left">
+                                <div class="icon icon-sm icon-secondary-1 index-list__counter"></div>
+                            </div>
+                            <div class="box__body">
+                                <h4><a href="#">Telephone</a></h4>
+                                <p>Voice communications have certainly changed over the years.</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+        </section>
+        <!-- END Products-->
+    </main>
+    <!--========================================================
+                              FOOTER
+    ==========================================================-->
+    <footer class="page-footer">
+        <div class="container text-center">
+            <p>
+                <a href="./">Connect</a> &copy; <span id="copyright-year"></span> &bull;
+                <span class="rd-navbar--switch-to-desktop"></span>
+                <a href='index-5.html'>Privacy Policy</a>
+            </p>
+        </div>
+    </footer>
+</div>
+
+<!-- Core Scripts -->
+<script src="js/core.min.js"></script>
+<!-- Additional Functionality Scripts -->
+<script src="js/script.js"></script>
+<!-- begin olark code -->
+<script data-cfasync="false" type='text/javascript'>/*<![CDATA[*/window.olark||(function(c){var f=window,d=document,l=f.location.protocol=="https:"?"https:":"http:",z=c.name,r="load";var nt=function(){
+f[z]=function(){
+(a.s=a.s||[]).push(arguments)};var a=f[z]._={
+},q=c.methods.length;while(q--){(function(n){f[z][n]=function(){
+f[z]("call",n,arguments)}})(c.methods[q])}a.l=c.loader;a.i=nt;a.p={
+0:+new Date};a.P=function(u){
+a.p[u]=new Date-a.p[0]};function s(){
+a.P(r);f[z](r)}f.addEventListener?f.addEventListener(r,s,false):f.attachEvent("on"+r,s);var ld=function(){function p(hd){
+hd="head";return["<",hd,"></",hd,"><",i,' onl' + 'oad="var d=',g,";d.getElementsByTagName('head')[0].",j,"(d.",h,"('script')).",k,"='",l,"//",a.l,"'",'"',"></",i,">"].join("")}var i="body",m=d[i];if(!m){
+return setTimeout(ld,100)}a.P(1);var j="appendChild",h="createElement",k="src",n=d[h]("div"),v=n[j](d[h](z)),b=d[h]("iframe"),g="document",e="domain",o;n.style.display="none";m.insertBefore(n,m.firstChild).id=z;b.frameBorder="0";b.id=z+"-loader";if(/MSIE[ ]+6/.test(navigator.userAgent)){
+b.src="javascript:false"}b.allowTransparency="true";v[j](b);try{
+b.contentWindow[g].open()}catch(w){
+c[e]=d[e];o="javascript:var d="+g+".open();d.domain='"+d.domain+"';";b[k]=o+"void(0);"}try{
+var t=b.contentWindow[g];t.write(p());t.close()}catch(x){
+b[k]=o+'d.write("'+p().replace(/"/g,String.fromCharCode(92)+'"')+'");d.close();'}a.P(2)};ld()};nt()})({
+loader: "static.olark.com/jsclient/loader0.js",name:"olark",methods:["configure","extend","declare","identify"]});
+/* custom configuration goes here (www.olark.com/documentation) */
+olark.identify('7830-582-10-3714');/*\]\]>*/</script><noscript><a href="https://www.olark.com/site/7830-582-10-3714/contact" title="Contact us" target="_blank">Questions? Feedback?</a> powered by <a href="http://www.olark.com?welcome" title="Olark live chat software">Olark live chat software</a></noscript>
+<!-- end olark code -->
+</body><!-- Google Tag Manager --><noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-P9FT69"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P9FT69');</script><!-- End Google Tag Manager -->
+</html>
