aa1fe2b9957d64dbb42c37ff9443b647e311155f
RolandoContreras <software.contreras@gmail.com>
2016-12-14 01:51:08 -0500

    changes in binary tree controller

--- application/controllers/b_binario.php
+++ application/controllers/b_binario.php
@@ -112,7 +112,7 @@ class B_binario extends CI_Controller {
                                             customer.status_value",
                                  "where" => "$str customer.created_at > '$creacion' and customer.status_value = 1",
                                  "join" => array('franchise, customer.franchise_id = franchise.franchise_id'),
-                                  "limit" => "50"
+                                  "limit" => "100"
                         ); 
                     $obj_tree = $this->obj_customer->search($param_tree); 
                     
@@ -152,9 +152,6 @@ class B_binario extends CI_Controller {
                         );             
 
             }
-            
-//            var_dump($n1);
-//            die();
                 
             foreach ($obj_tree as $key => $value) {
                 
