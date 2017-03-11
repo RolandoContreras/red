996c78c0c6707117a41e065a9aafba0fa53bf0f8
RolandoContreras <software.contreras@gmail.com>
2017-03-11 16:56:09 -0500

    add ruc on all view

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,11 +1,15 @@
     <footer class="page-footer">
         <div class="container text-center">
             <p>
+                <a style="color:#1db8ea;">Inversiones BitShare S.A.C.&nbsp;&nbsp;RUC: 20601940087</a></p>
+            </p>
+            <p>
                 <a href="./">BitShare</a> &copy; <span id="copyright-year"></span> &bull;
                 <span class="rd-navbar--switch-to-desktop"></span>
-                <a href=''>Privacy Policy</a>
+                Privacy Policy
                 <!-- {%FOOTER_LINK} -->
             </p>
+            
             <script type="text/javascript" src="https://cdn.ywxi.net/js/1.js" async></script>
         </div>
     </footer>
