937bb15ae4c93a7231400dcc38b6465041489d73
RolandoContreras <software.contreras@gmail.com>
2017-03-13 23:44:17 -0500

    changes in faq

--- application/views/faq.php
+++ application/views/faq.php
@@ -20,122 +20,43 @@
             <div class="container">
                 <h1><?php echo replace_vocales_voculeshtml("Preguntas Frecuentes - Bitshare FAQs");?></h1>
                 <div class="row">
-                    <div class="col-md-12 text-center">
-                                    
-                        <p style="text-align: left;"><strong><?php echo replace_vocales_voculeshtml("Bitshare es una empresa de Multinivel?");?></strong></p><br>    
-                        <!--<img src="<?php echo site_url().'static/page_front/images/empresa.jpg';?>" alt="misión" width="370" height="279">-->
+                    <div class="col-md-12 text-left" style="padding-bottom: 10px !important;">
+                        <p><strong><?php echo replace_vocales_voculeshtml("¿Bitshare es una empresa de Multinivel?");?></strong></p>  
+                         <p><?php echo replace_vocales_voculeshtml("-Sí, bitshare es una tienda Online que trabaja bajo el sistema de multinivel con un concepto revolucionario de negocio donde comercializamos oro en pequeñas cantidades.");?></p><br>
                     </div>
-                    <div class="col-md-12">
-                        <p><?php echo replace_vocales_voculeshtml("Nuestra misión es ayudar a capitalizar y dar herramientas para un crecimiento económico y personal a los asociados en este nuevo cambio de era a través de la comercialización y el  buen manejo del bitcoin y los metales preciosos; convirtiéndose cada una de ellos en los nuevos emprendedores.");?></p><br>
-                        <p><?php echo replace_vocales_voculeshtml("Nuestra visión consiste en lograr el liderazgo en el rubro de criptomoneda y metales preciosos mediante un ecosistema sano creado  por los asociados. BitShare tiene como objetivo dar herramientas y ayudar a sus miles de clientes a lograr que los pagos sean más fáciles y rápidos en todo el mundo.");?></p>
+                    <div class="col-md-12 text-left" style="padding-bottom: 10px !important;">
+                        <p><strong><?php echo replace_vocales_voculeshtml("¿Bitshare es una empresa legal?");?></strong></p>  
+                         <p><?php echo replace_vocales_voculeshtml("-Sí, bitshare es una empresa legal registrada en la república del Perú con la siguiente denominación:");?></p>
+                         <p><?php echo replace_vocales_voculeshtml("Razón Social: INVERSIONES BITSHARE S.A.C    .");?></p>
+                         <p><?php echo replace_vocales_voculeshtml("Número de Ruc: 20601940087");?></p><br>
                     </div>
-                </div>
-            </div>
-        </section>
-        <!-- END Our mission-->
-        <!-- Why Us -->
-        <section class="relative well-lg well-lg-inset-1">
-            <div class="absolute wow fadeInRight">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="image-wrap"><img src="<?php echo site_url().'static/page_front/images/page-02_img06.jpg';?>" width="1010" height="1125" alt=""></div>
+                    <div class="col-md-12 text-left" style="padding-bottom: 10px !important;">
+                        <p><strong><?php echo replace_vocales_voculeshtml("¿Quiénes son los dueños de bitshare?");?></strong></p>  
+                         <p><?php echo replace_vocales_voculeshtml("-Son 3 dueños quienes están al frente de compañía bitshare:");?></p>
+                         <p><i><?php echo replace_vocales_voculeshtml("1) Dr. Carlos Gómez: Presidente de la cooperativa de los más importante mineros independiente del Perú. Empresario de minas de oro, plata y cobre.");?></p>
+                         <p><?php echo replace_vocales_voculeshtml("2) Silene Carrasco: Networker de la industria por más de 10 años de experiencia con grandes resultados y a la vez ejecutiva de administración y negocios internacionales.");?></p>
+                         <p><?php echo replace_vocales_voculeshtml("3) Rolando Contreras: Experto en informática y desarrollador de software para grandes empresas a nivel internacional y a la vez networker de la industria MLM.");?></p><br>
                     </div>
-                </div>
-            </div>
-
-            <div class="container wow fadeInLeft">
-                <div class="row">
-                    <div class="col-md-preffix-7 col-md-5">
-                        <h1><?php echo replace_vocales_voculeshtml("¿Porqué Nosotros?");?></h1>
-                        <p class="inset-2"><?php echo replace_vocales_voculeshtml("Al ingresar en Bitshare Ud. está comprando oro, dicho oro es comercializado a través de nuestra empresa para la venta de forma minorista teniendo una buena rentabilidad y también obtenemos ganancias por el alza del precio de este metal precioso. Hacemos trading de bitcoin (compra y venta) y parte de las inversiones de BITSHARE son hechas en esta criptomoneda y debido a la gran demanda del mercado se estima que en estos años el precio suba sobre los $1000 por moneda dando una mayor seguridad.");?>
-                    </p>
+                    <div class="col-md-12 text-left" style="padding-bottom: 10px !important;">
+                        <p><strong><?php echo replace_vocales_voculeshtml("¿Por qué bitshare no es una pirámide financiera o un esquema ponzi?");?></strong></p>  
+                         <p><?php echo replace_vocales_voculeshtml("1) Bitshare no trabaja dinero por dinero.");?></p>
+                         <p><?php echo replace_vocales_voculeshtml("2) Tenemos un producto físico y tangible que vendemos.");?></p>
+                         <p><?php echo replace_vocales_voculeshtml("3) No es necesario reclutar nuevos miembros para mantener o sostener la compañía; bitshare tiene una ganancia por la comercialización de la compra y venta del oro.");?></p>
+                         <p><?php echo replace_vocales_voculeshtml("4) Todas las personas tienen las mismas opciones de ganancia y no tan solo los primeros.");?></p>
+                         <p><?php echo replace_vocales_voculeshtml("5) Para cobrar las comisiones no es necesario traer a nuevos socios a la compañía para venderlas, etc.");?></p><br>
                     </div>
-                </div>
-            </div>
-        </section>
-        <!-- END Why Us-->
-
-        <!-- Products -->
-        <section class="well-md well-md-inset-1 bg-primary text-center">
-            <div class="container wow fadeInUp">
-                <h1>Empresas</h1>
-                <ul class="index-list row">
-                    <li class="col-md-6">
-                        <div class="box">
-                            <div class="box__left">
-                                <div class="icon icon-sm icon-secondary-1 index-list__counter"></div>
-                            </div>
-                            <div class="box__body">
-                                <h4><a href="#">Trading</a></h4>
-                                <p><?php echo replace_vocales_voculeshtml("Hacemos compra y venta de bitcoin no solamente a personas de BitShare sino también a empresarios externos.");?></p>
-                            </div>
-                        </div>
-                    </li>
+                    <div class="col-md-12 text-left" style="padding-bottom: 10px !important;">
+                        <p><strong><?php echo replace_vocales_voculeshtml("¿Cuánto es la comisión por sacar nuestro dinero a otros monederos externos de bitcoin?");?></strong></p>  
+                         <p><?php echo replace_vocales_voculeshtml("-El 7% del monto enviado por gastos administrativos y operacionales.");?></p><br>
+                    </div>                        
+                    <div class="col-md-12 text-left" style="padding-bottom: 10px !important;">
+                        <p><strong><?php echo replace_vocales_voculeshtml("¿Cuánto es la comisión por sacar nuestro dinero a otros monederos externos de bitcoin?");?></strong></p>  
+                         <p><?php echo replace_vocales_voculeshtml("-El 7% del monto enviado por gastos administrativos y operacionales.");?></p><br>
+                    </div>                        
                     
-                    <li class="col-md-6">
-                        <div class="box">
-                            <div class="box__left">
-                                <div class="icon icon-sm icon-secondary-1 index-list__counter"></div>
-                            </div>
-                            <div class="box__body">
-                                <h4><a href="#">Oro</a></h4>
-                                <p><?php echo replace_vocales_voculeshtml("Primera empresa que comercializa oro en pequeñas cantidades (minorista) ganando posicionamiento en el mercado y generando una buena rentabilidad a BITSHARE.");?></p>
-                            </div>
-                        </div>
-                    </li>
-
-<!--                    <li class="col-md-4">
-                        <div class="box">
-                            <div class="box__left">
-                                <div class="icon icon-sm icon-secondary-1 index-list__counter"></div>
-                            </div>
-                            <div class="box__body">
-                                <h4><a href="#">Pesca</a></h4>
-                                <p><?php echo replace_vocales_voculeshtml("Manejamos embarcaciones pesqueras por más de 5 años con buenas rentabilidades anuales.");?></p>
-                            </div>
-                        </div>
-                    </li>-->
-
-<!--                    <li class="col-md-4">
-                        <div class="box" style="margin-top: 40px !important;">
-                            <div class="box__left">
-                                <div class="icon icon-sm icon-secondary-1 index-list__counter"></div>
-                            </div>
-                            <div class="box__body">
-                                <h4><a href="#">Casas de Cambio</a></h4>
-                                <p><?php echo replace_vocales_voculeshtml("Diversificamos en este rubro que está en constante crecimiento y muy atractivo.");?></p>
-                            </div>
-                        </div>
-                    </li>-->
-<!--                    
-                    <li class="col-md-4">
-                        <div class="box" style="margin-top: 40px !important;">
-                            <div class="box__left">
-                                <div class="icon icon-sm icon-secondary-1 index-list__counter"></div>
-                            </div>
-                            <div class="box__body">
-                                <h4><a href="#">Importaciones</a></h4>
-                                <p><?php echo replace_vocales_voculeshtml("Comercializamos contenedores desde China y Panamá por más de 2 años con un equipo profesional y experto en el tema.");?></p>
-                            </div>
-                        </div>
-                    </li>-->
-<!--                    
-                    <li class="col-md-4">
-                        <div class="box" style="margin-top: 40px !important;">
-                            <div class="box__left">
-                                <div class="icon icon-sm icon-secondary-1 index-list__counter"></div>
-                            </div>
-                            <div class="box__body">
-                                <h4><a href="#">Software</a></h4>
-                                <p><?php echo replace_vocales_voculeshtml("Hace más de 7 años nos dedicamos a la construcción de sistemas para empresas y desarrollo de software a nivel internacional.");?></p>
-                            </div>
-                        </div>
-                    </li>-->
-                </ul>
+                </div>
             </div>
-
         </section>
-        <!-- END Products-->
     </main>
     <!--========================================================
                               FOOTER
