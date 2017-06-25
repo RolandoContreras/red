190d91f73631285df2bdb1be7cd98a832eaa18e6
software.contreras@gmail.com <software.contreras@gmail.com>
2017-06-25 15:17:59 -0500

    changes data baskcoffice

--- application/views/backoffice/b_data.php
+++ application/views/backoffice/b_data.php
@@ -7,6 +7,9 @@
             <a class="white"><?php echo "Precio del BITCOIN: "?><?php echo $price_btc;?></a>
         </div>
     </div> 
+    
+   
+    
          <!-- Page content-->
          <!--<div class="content-wrapper">-->
             <div class="row">
@@ -14,6 +17,29 @@
                   <div class="panel panel-info">
                       <div class="panel-heading text-uppercase">usuario: <b><?php echo $obj_customer->username;?></b></div>
                      <div class="panel-body">
+                         
+                         
+                         <div class="tab">
+                                <button class="tablinks" onclick="openCity(event, 'London')">INFORMACION PRINCIPAL</button>
+                                <button class="tablinks" onclick="openCity(event, 'Tokyo')">INFORMACION BANCARIA</button>
+                                <button class="tablinks" onclick="openCity(event, 'Paris')">CONTRASEÑAS</button>
+                                
+                              </div>
+
+                              <div id="London" class="tabcontent">
+                                <h3>London</h3>
+                                <p>London is the capital city of England.</p>
+                              </div>
+
+                              <div id="Paris" class="tabcontent">
+                                <h3>Paris</h3>
+                                <p>Paris is the capital of France.</p> 
+                              </div>
+
+                              <div id="Tokyo" class="tabcontent">
+                                <h3>Tokyo</h3>
+                                <p>Tokyo is the capital of Japan.</p>
+                              </div> 
                         <form name="FormEditarAfiliados" class="form-horizontal" data-parsley-validate>
                            <div class="col-lg-4">
                               <legend>Datos Principales</legend>
@@ -226,3 +252,64 @@
          <!--</div>-->
       </section>
 <script src="<?php echo site_url().'static/backoffice/js/data.js';?>"></script>
+<script>
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+</script>
+<style>
+    /* Style the tab */
+div.tab {
+    overflow: hidden;
+    border: 1px solid #ccc;
+    background-color: #f1f1f1;
+}
+
+/* Style the buttons inside the tab */
+div.tab button {
+    background-color: inherit;
+    float: left;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 14px 16px;
+    transition: 0.3s;
+}
+
+/* Change background color of buttons on hover */
+div.tab button:hover {
+    background-color: #ddd;
+}
+
+/* Create an active/current tablink class */
+div.tab button.active {
+    background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+    display: none;
+    padding: 6px 12px;
+    border: 1px solid #ccc;
+    border-top: none;
+}
+</style>
+    
+    
